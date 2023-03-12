b6ac5c895e8acfa6a2cebc5872ac7f077f2aa903
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-12 14:52:58 +0100

    feature: added-temp-data-helper-static-class

---
+++ RentalCompany.Utility/TempDataHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace RentalCompany.Utility;
+public static class TempDataHelper
+{
+    public static void SetSuccess(Controller controller, string message)
+    {
+        controller.TempData["success"] = message;
+    }
+}

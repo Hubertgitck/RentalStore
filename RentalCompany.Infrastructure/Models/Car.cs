b0f7d476e30a7e187295364b038d1a495445d534
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-20 13:29:45 +0100

    added-database.png

--- RentalCompany.Infrastructure/Models/Car.cs
+++ RentalCompany.Infrastructure/Models/Car.cs
@@ -4,8 +4,10 @@ public class Car
     [Key]
     public int Id { get; set; }
     [Required]
+    [StringLength(50)]
     public string Name { get; set; }
     [Required]
+    [StringLength(100)]
     public string Description { get; set; }
     [Required]
     public double DayRentalPrice { get; set; }
